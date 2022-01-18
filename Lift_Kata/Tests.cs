808af010c81704f29547766ad5affcfd27a7db25
Raúl Villares <raulvillares@gmail.com>
2022-01-18 15:25:26 +0100

    Initial commit

---
+++ Lift_Kata/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Lift_Kata
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
